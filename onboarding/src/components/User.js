4f2b9ccaa24e6efb9fdc8cdb87d51680ae93506b
Stacey Louis <stacey-louis@lambdastudents.com>
2020-07-23 14:37:10 -0400

    minor cosmetic changes

--- onboarding/src/components/User.js
+++ onboarding/src/components/User.js
@@ -1,13 +1,17 @@
 
 import React from 'react'
+import logo from './form-photo.jpg';
+import '../App.css'
 
 function User({ values }) {
+  
   if (!values) {
     return <h3>No Info available</h3>
   }
 
   return (
     <div className='user-container'>
+      <img id="avatar" src={values.avatar || logo}/>
       <h2>Name:{values.first_name} {values.last_name}</h2>
       <p>Email: {values.email}</p>
       <p>Password: {values.password}</p>
