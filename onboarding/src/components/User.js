ca273249b4a1fc4dfeec4c385aa8f8990d8594ea
Stacey Louis <stacey-louis@lambdastudents.com>
2020-07-23 14:26:26 -0400

    minor fixes

--- onboarding/src/components/User.js
+++ onboarding/src/components/User.js
@@ -8,7 +8,7 @@ function User({ values }) {
 
   return (
     <div className='user-container'>
-      <h2>Name:{values.first_name, values.last_name || values.name}</h2>
+      <h2>Name:{values.first_name} {values.last_name}</h2>
       <p>Email: {values.email}</p>
       <p>Password: {values.password}</p>
     
